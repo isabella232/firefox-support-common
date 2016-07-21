71c6b8b81231284ac1c39cff7fc78cd1acf71437
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-07-21 17:13:57 +0900

    Format headers

--- verification_manual/tools/mdformat.rb
+++ verification_manual/tools/mdformat.rb
@@ -1,7 +1,36 @@
 #!/usr/bin/env ruby
 
-md = File.read(ARGV[0])
-md = md.gsub(/^(\d+\.\d+[^\n]*)\n-{10,}\n/, "## \\1\n")
-md = md.gsub(/^(\d+[^\n]*)\n={10,}\n/, "# \\1\n")
-#md = md.gsub(/^(\d+\.\d+)([^ \d]+)$/, "\\1 \\2")
-puts md
+class Markdown
+  def initialize(path)
+    @text = File.read(path)
+  end
+
+  def format!
+    format_headers!
+    format_spaces!
+    remove_garbages!
+  end
+
+  def format_headers!
+    @text.gsub!(/^(\d+\.\d+[^\n]*)\n-{10,}\n/, "## \\1\n")
+    @text.gsub!(/^(\d+[^\n]*)\n={10,}\n/, "# \\1\n")
+  end
+
+  def format_spaces!
+    @text.gsub!(/^(\d+\.\d+)([^ \d]+)$/, "\\1 \\2")
+    @text.gsub!(/^(#+)(\d+)$/, "\\1 \\2")
+    @text.gsub!(/^(#+ \d+(?:\.\d+)*)[[:space:]]*([^ \.\d])/, "\\1 \\2")
+  end
+
+  def remove_garbages!
+    @text.gsub!(/^(#.*) +{.*}$/, "\\1")
+  end
+
+  def to_s
+    @text
+  end
+end
+
+md = Markdown.new(ARGV[0])
+md.format!
+puts md.to_s
