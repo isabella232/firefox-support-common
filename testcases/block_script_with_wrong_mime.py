96b5e0b1d1f0659e8d7dcaddb67f3d4a7da2418c
Kentaro Hayashi <hayashi@clear-code.com>
2018-03-13 19:24:42 +0900

    Add testcase for Security-30

---
+++ testcases/block_script_with_wrong_mime.py
@@ -0,0 +1,18 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+server_address = ('localhost', 8888)
+
+# Return JavaScript with an invalid MIME type
+SimpleHTTPRequestHandler.extensions_map['.js'] = 'image/png'
+
+SimpleHTTPRequestHandler.protocol_version = "HTTP/1.0"
+httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+
+sa = httpd.socket.getsockname()
+print("Serving HTTP on", sa[0], "port", sa[1], "...")
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("\nKeyboard interrupt received, exiting.")
+    httpd.server_close()
+    sys.exit(0)
